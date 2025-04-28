847d66357c8897342de68e3e1ce329f8a31c5f35
user <gorod.eg08@gmail.com>
2025-04-28 21:45:44 +0500

    5th commit, server

--- bot.py
+++ bot.py
@@ -129,6 +129,7 @@ def run_bot():
 
 
 if __name__ == "__main__":
+
     bot_thread = threading.Thread(target=run_bot)
     bot_thread.start()
     app.run(host='0.0.0.0', port=3000)
--- cities.py
+++
@@ -1,9 +0,0 @@
-import sqlite3
-a = 'бузулук'
-con = sqlite3.connect("cities.db")
-cur = con.cursor()
-result = cur.execute(f"""SELECT city FROM cities
-            WHERE city LIKE '{a}'""").fetchall()
-s = [i[0] for i in result]
-print(s)
-con.close()
